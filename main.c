58f98b543eed2cc1d5f20031d83824cfdd24d47b
SrVariable <rojohnibana@gmail.com>
2024-05-06 12:13:17 +0200

    Refactor code

--- main.c
+++ main.c
@@ -10,10 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#define WIDTH 50
-#define HEIGHT 25
-#define PIXEL_SIZE 16
-#define UPDATE_RATE 11
+#define WIDTH 75		// Width of the board
+#define HEIGHT 40		// Height of the board
+#define PIXEL_SIZE 16 	// Size of each pixel
+#define UPDATE_RATE 11	// Update rate of the board
+
+// Note: These colors can be replaced using the function GetColor()
+// Example color yellow (RGBA Hex): GetColor(0xFFFF00FF)
+#define FOREGROUND_COLOR RED	// GOL's foreground color
+#define BACKGROUND_COLOR BLACK	// GOL's background color
+#define CURSOR_COLOR YELLOW		// GOL's cursor color
 
 #include "raylib.h"
 #include "raymath.h"
@@ -30,7 +36,7 @@ static void	initialise_scene(Camera2D *camera)
 	HideCursor();
 }
 
-static int	count_neighbours(bool cell[WIDTH][HEIGHT], Vector2 position)
+static int	count_neighbours(bool board[WIDTH][HEIGHT], Vector2 position)
 {
 	int	neighbour;
 	int	x;
@@ -46,14 +52,14 @@ static int	count_neighbours(bool cell[WIDTH][HEIGHT], Vector2 position)
 			if (i == 0 && j == 0)
 				continue;
 			if ((x + i >= 0 && x + i < WIDTH) && (y + j >= 0 && y + j < HEIGHT)
-				&& cell[x + i][y + j])
+				&& board[x + i][y + j])
 				++neighbour;
 		}
 	}
 	return (neighbour);
 }
 
-static void	update_board(bool cell[WIDTH][HEIGHT])
+static void	update_board(bool board[WIDTH][HEIGHT])
 {
 	static bool temp[WIDTH][HEIGHT];
 	int	neighbours;
@@ -62,37 +68,29 @@ static void	update_board(bool cell[WIDTH][HEIGHT])
 	{
 		for (int j = 0; j < HEIGHT; ++j)
 		{
-			neighbours = count_neighbours(cell, (Vector2){i, j});
+			neighbours = count_neighbours(board, (Vector2){i, j});
 			if (neighbours == 3)
 				temp[i][j] = true;
 			else if (neighbours == 2)
-				temp[i][j] = cell[i][j];
+				temp[i][j] = board[i][j];
 			else
 				temp[i][j] = false;
 		}
 	}
 	for (int i = 0; i < WIDTH; ++i)
-		memcpy(cell[i], temp[i], HEIGHT * sizeof(bool));
+		memcpy(board[i], temp[i], HEIGHT * sizeof(bool));
 }
 
-static void	draw_board(bool cell[WIDTH][HEIGHT])
+static void	draw_board(bool board[WIDTH][HEIGHT])
 {
 	for (int i = 0; i < WIDTH; ++i)
 		for (int j = 0; j < HEIGHT; ++j)
-			if (cell[i][j])
+			if (board[i][j])
 				DrawRectangle(i * PIXEL_SIZE, j * PIXEL_SIZE,
-								PIXEL_SIZE, PIXEL_SIZE, RED);
+								PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
 }
 
-static void	gol(bool cell[WIDTH][HEIGHT])
-{
-	for (int i = 0; i < WIDTH; ++i)
-		for (int j = 0; j < HEIGHT; ++j)
-			if (i % 2 == 0 && j % 2 == 0)
-				cell[i][j] = false;
-}
-
-static void	put_glider(bool cell[WIDTH][HEIGHT], Vector2 position)
+static void	put_glider(bool board[WIDTH][HEIGHT], Vector2 position)
 {
 	int	x;
 	int	y;
@@ -101,11 +99,11 @@ static void	put_glider(bool cell[WIDTH][HEIGHT], Vector2 position)
 	y = (int)position.y;
 	if ((x < 0 || x + 2 >= WIDTH) && (y < 0 || y + 2 >= HEIGHT))
 		return ;
-	cell[x + 2][y + 0] = true;
-	cell[x + 2][y + 1] = true;
-	cell[x + 2][y + 2] = true;
-	cell[x + 1][y + 2] = true;
-	cell[x + 0][y + 1] = true;
+	board[x + 2][y + 0] = true;
+	board[x + 2][y + 1] = true;
+	board[x + 2][y + 2] = true;
+	board[x + 1][y + 2] = true;
+	board[x + 0][y + 1] = true;
 }
 
 static void	draw_glider(Vector2 position)
@@ -117,14 +115,14 @@ static void	draw_glider(Vector2 position)
 	y = (int)position.y;
 	if ((x < 0 || x + 2 >= WIDTH) && (y < 0 || y + 2 >= HEIGHT))
 		return ;
-	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 0) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 2) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 1) * PIXEL_SIZE, (y + 2) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 0) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
+	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 0) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 2) * PIXEL_SIZE, (y + 2) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 1) * PIXEL_SIZE, (y + 2) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 0) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
 }
 
-static void	put_blinker(bool cell[WIDTH][HEIGHT], Vector2 position)
+static void	put_blinker(bool board[WIDTH][HEIGHT], Vector2 position)
 {
 	int	x;
 	int	y;
@@ -133,9 +131,9 @@ static void	put_blinker(bool cell[WIDTH][HEIGHT], Vector2 position)
 	y = (int)position.y;
 	if ((x < 0 || x + 2 >= WIDTH) && (y < 0 || y + 2 >= HEIGHT))
 		return ;
-	cell[x + 0][y] = true;
-	cell[x + 1][y] = true;
-	cell[x + 2][y] = true;
+	board[x + 0][y] = true;
+	board[x + 1][y] = true;
+	board[x + 2][y] = true;
 }
 
 static void	draw_blinker(Vector2 position)
@@ -147,52 +145,56 @@ static void	draw_blinker(Vector2 position)
 	y = (int)position.y;
 	if ((x < 0 || x + 2 >= WIDTH) && y < 0)
 		return ;
-	DrawRectangle((x + 0) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 1) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
-	DrawRectangle((x + 2) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, RED);
+	DrawRectangle((x + 0) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 1) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
+	DrawRectangle((x + 2) * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, FOREGROUND_COLOR);
 }
 
-static void	click_cell(bool cell[WIDTH][HEIGHT])
+static void	clear_board(bool board[WIDTH][HEIGHT])
+{
+	for (int i = 0; i < WIDTH; ++i)
+		memset(board[i], false, HEIGHT);
+}
+
+static void	click_cell(bool board[WIDTH][HEIGHT])
 {
 	int	x;
-	int y;
+	int	y;
 
 	x = GetMouseX() / PIXEL_SIZE;
 	y = GetMouseY() / PIXEL_SIZE;
 	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
 		return ;
-	cell[x][y] = !cell[x][y];
+	board[x][y] = !board[x][y];
 }
 
 int	main(void)
 {
 	Camera2D		camera;
-	static bool		cell[WIDTH][HEIGHT];
-	static int		frame_count;
+
+	static bool		board[WIDTH][HEIGHT];
+	static int		frameCount;
 	static bool		isPaused = true;
 	static int		delay;
-
-	gol(cell);
 
 	InitWindow(WIDTH * PIXEL_SIZE, HEIGHT * PIXEL_SIZE, "GOL");
 	initialise_scene(&camera);
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(BACKGROUND_COLOR);
 		BeginMode2D(camera);
-		draw_board(cell);
+		draw_board(board);
 		EndMode2D();
 		if (isPaused)
-			DrawText("Press space to play",
-					WIDTH / 3 * PIXEL_SIZE, HEIGHT / 2 * PIXEL_SIZE,
-					32, WHITE);
+			DrawText("Paused", WIDTH * PIXEL_SIZE - (8 * PIXEL_SIZE), 0,
+						32, WHITE);
 		if (IsKeyDown(KEY_G))
 		{
 			draw_glider((Vector2){GetMouseX() / PIXEL_SIZE,
 									GetMouseY() / PIXEL_SIZE});
 			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-				put_glider(cell, (Vector2){GetMouseX() / PIXEL_SIZE,
+				put_glider(board, (Vector2){GetMouseX() / PIXEL_SIZE,
 											GetMouseY() / PIXEL_SIZE});
 		}
 		else if (IsKeyDown(KEY_B))
@@ -200,25 +202,27 @@ int	main(void)
 			draw_blinker((Vector2){GetMouseX() / PIXEL_SIZE,
 									GetMouseY() / PIXEL_SIZE});
 			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-				put_blinker(cell, (Vector2){GetMouseX() / PIXEL_SIZE,
+				put_blinker(board, (Vector2){GetMouseX() / PIXEL_SIZE,
 											GetMouseY() / PIXEL_SIZE});
 		}
-		else
-			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-				click_cell(cell);
-		DrawCircle(GetMouseX(), GetMouseY(), PIXEL_SIZE / 4, YELLOW);
-		EndDrawing();
-		if (IsKeyPressed(KEY_SPACE))
+		else if (IsKeyPressed(KEY_SPACE))
 			isPaused = !isPaused;
 		else if (IsKeyDown(KEY_COMMA) && UPDATE_RATE + delay < 20)
 			++delay;
 		else if (IsKeyDown(KEY_PERIOD) && UPDATE_RATE + delay > 1)
 			--delay;
-		if (++frame_count % (UPDATE_RATE + delay) == 0 && !isPaused)
+		else if (IsKeyDown(KEY_R))
+			clear_board(board);
+		else
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+				click_cell(board);
+		DrawCircle(GetMouseX(), GetMouseY(), PIXEL_SIZE / 4, CURSOR_COLOR);
+		EndDrawing();
+		if (++frameCount % (UPDATE_RATE + delay) == 0 && !isPaused)
 		{
-			update_board(cell);
-			if (frame_count == 10000)
-				frame_count = 0;
+			update_board(board);
+			if (frameCount == 10000)
+				frameCount = 0;
 		}
 	}
 	CloseWindow();
